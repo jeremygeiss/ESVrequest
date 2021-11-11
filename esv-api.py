0b8157406ae81b398f4563bdb3098d2ee94e80a6
Jeremy Geiss <jeremy.geiss@gmail.com>
2021-11-10 21:28:17 -0600

    initial commit

---
+++ esv-api.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import sys
+import requests
+
+
+API_KEY = '6d222046d13872627d6653c6325934474dfbd46f'
+API_URL = 'https://api.esv.org/v3/passage/text/'
+
+
+def get_esv_text(passage):
+    params = {
+        'q': passage,
+        'include-headings': True,
+        'include-footnotes': False,
+        'include-verse-numbers': True,
+        'include-short-copyright': True,
+        'include-passage-references': True
+    }
+
+    headers = {
+        'Authorization': 'Token %s' % API_KEY
+    }
+
+    response = requests.get(API_URL, params=params, headers=headers)
+
+    passages = response.json()['passages']
+
+    return passages[0].strip() if passages else 'Error: Passage not found'
+
+
+if __name__ == '__main__':
+    passage = ' '.join(sys.argv[1:])
+
+    if passage:
+        print(get_esv_text(passage))
